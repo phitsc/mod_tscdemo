d857a6248f89562e31a17cab185c2deaac0e5b5e
phitsc <phitsc@gmail.com>
2017-09-08 12:07:33 +0200

    Add reference

--- build++.sh
+++ build++.sh
@@ -1,3 +1,4 @@
+# See https://stackoverflow.com/questions/42605138/how-can-i-write-an-apache-module-in-c
 
 g++ -std=c++11 -DLINUX -D_GNU_SOURCE -D_REENTRANT -fPIC -I/usr/local/apache2/include -I/usr/include/apr-1.0 -pthread -pedantic-errors -Werror -o out/mod_tscdemo.o -c src/mod_tscdemo.cpp
 
