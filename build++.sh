3f6901755392e5622aadf97df47258b593f5e2c1
phitsc <phitsc@gmail.com>
2017-09-08 12:07:20 +0200

    No need to execute apxs if compilation fails

--- build++.sh
+++ build++.sh
@@ -1,3 +1,6 @@
 
 g++ -std=c++11 -DLINUX -D_GNU_SOURCE -D_REENTRANT -fPIC -I/usr/local/apache2/include -I/usr/include/apr-1.0 -pthread -pedantic-errors -Werror -o out/mod_tscdemo.o -c src/mod_tscdemo.cpp
+
+[ $? -eq 0 ] || exit $?;
+
 apxs -ia -n tscdemo -lapr-1 -lstdc++ -c out/mod_tscdemo.o
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 
 gcc -std=gnu99 -DLINUX -D_GNU_SOURCE -D_REENTRANT -fPIC -I/usr/local/apache2/include -I/usr/include/apr-1.0 -pthread -pedantic-errors -Werror -o out/mod_tscdemo.o -c src/mod_tscdemo.c
+
+[ $? -eq 0 ] || exit $?;
+
 apxs -ia -n tscdemo -lapr-1 -c out/mod_tscdemo.o
